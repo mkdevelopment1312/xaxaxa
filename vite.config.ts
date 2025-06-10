658ba404e4544c10bdff3783a400d871db083cbd
Twoja Nazwa <twoj_email@example.com>
2025-06-10 16:53:37 +0200

    Poprawki w konfiguracji routingu i builda

--- vite.config.ts
+++ vite.config.ts
@@ -11,9 +11,12 @@ export default defineConfig({
     componentTagger(),
   ],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+    alias: [
+      {
+        find: "@",
+        replacement: path.resolve(__dirname, "./src"),
+      },
+    ],
   },
   server: {
     host: "::",
