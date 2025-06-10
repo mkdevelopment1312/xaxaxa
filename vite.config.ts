0c59ef56045eafeca7c90376a88ac35afcbd85e8
Twoja Nazwa <twoj_email@example.com>
2025-06-10 20:31:33 +0200

    Remove lovable dependencies and replace with xaxa branding

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/xaxaxa/',
   plugins: [
     react(),
-    componentTagger(),
   ],
   resolve: {
     alias: [
